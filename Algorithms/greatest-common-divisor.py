c8ff2ee39953c47fca3861cf2e24d132e5ae9f7a
memeyankm <65050847+memeyankm@users.noreply.github.com>
2020-10-02 02:40:50 +0530

    Update and rename gcd.py to greatest-common-divisor.py

--- Algorithms/greatest-common-divisor.py
+++ Algorithms/greatest-common-divisor.py
@@ -1,7 +1,7 @@
 # Uses python3
 import sys
 
-def gcd_naive(a, b):
+def gcd(a, b):
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
         if a % d == 0 and b % d == 0:
@@ -13,4 +13,4 @@ def gcd_naive(a, b):
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    print(gcd(a, b))
