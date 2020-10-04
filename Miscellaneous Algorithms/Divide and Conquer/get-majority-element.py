9b50bcd9df9be47d117365b21422d3e692ce628d
memeyankm <65050847+memeyankm@users.noreply.github.com>
2020-10-04 07:24:55 +0530

    Update get-majority-element.py

--- Miscellaneous Algorithms/Divide and Conquer/get-majority-element.py
+++ Miscellaneous Algorithms/Divide and Conquer/get-majority-element.py
@@ -1,9 +1,11 @@
 #python3
 import sys
 
+#defining the function
 def get_majority_element(a, n):
     m = {}
 
+#collecting the elements
     for i in range(n):
         if a[i] in m:
             m[a[i]] += 1
@@ -12,14 +14,18 @@ def get_majority_element(a, n):
 
     count = 0
 
+#checking for the majority element
     for key in m:
         if m[key] > n / 2:
             return 1
 
+#returning -1 if there is no majority element
     if (count == 0):
         return -1
 
-
+#defining the input function
+#taking input from the user in correct format
+#refer readme
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
