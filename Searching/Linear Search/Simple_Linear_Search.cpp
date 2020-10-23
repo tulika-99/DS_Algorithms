4f8a9e991cf94552677e7ba326bd0b2db3663c76
Karan Dhingra <dhingrakaran764@gmail.com>
2020-10-23 11:22:25 +0530

    Simple way to do Linear Search in C++

---
+++ Searching/Linear Search/Simple_Linear_Search.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[1000];
+    int i, item, size, flag = 0;
+
+    cout<<"Enter the number of elements: ";
+
+    cin>>size;
+    for(int i=0; i<size; i++)
+    {
+        cout<<"\nEnter element as "<<i+1<<" position: ";
+        cin>>a[i];
+    }
+
+    cout<<"\nEnter the item you want to search: ";
+    cin>>item;
+
+    for(i=0; i<size; i++)
+    {
+        if(a[i] == item)
+        {
+            flag = 1;
+
+            break;
+        }
+    }
+
+    if(flag == 1)
+    cout<<"\nItem Found at "<<i+1<<" position.";
+
+    else 
+    cout<<"Item is not available";
+    return 0;
+}
