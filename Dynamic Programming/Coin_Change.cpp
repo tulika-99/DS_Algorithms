1b0e84f81c42a34e0a998931c73539b7a287f71e
simonjoy <56128356+simonjoy@users.noreply.github.com>
2020-10-24 15:46:51 +0530

    Create Coin_Change.cpp

---
+++ Dynamic Programming/Coin_Change.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int count( int S[], int m, int n ) 
+{ 
+	int i, j, x, y; 
+	int table[n + 1][m]; 
+	for (i = 0; i < m; i++) 
+		table[0][i] = 1; 
+	for (i = 1; i < n + 1; i++) 
+	{ 
+		for (j = 0; j < m; j++) 
+		{ 
+			x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0; 
+			y = (j >= 1) ? table[i][j - 1] : 0; 
+			table[i][j] = x + y; 
+		} 
+	} 
+	return table[n][m - 1]; 
+} 
+int main() 
+{
+  int m,n;
+  cin>>m;         //size of array
+  for(int i=0;i<m;i++)
+  {
+    cin>>a[i];
+  }
+  cin>>n; 
+	cout << count(arr, m, n); 
+	return 0; 
+}
