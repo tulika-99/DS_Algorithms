9a952e95a9d95827c8fcb9e12a2e224105d24aea
shrishtisrivastava <shrishtisrivastava04@gmail.com>
2020-10-27 22:03:15 +0530

    reverse a linked list with group of k

---
+++ Linked Lists/Reverse_a_linked_list_in_group_of_size_k.cpp
@@ -0,0 +1,99 @@
+/*.....reverse a liked list in group of size k.*/
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct node *reverse (struct node *head, int k);
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        head = reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+
+
+struct node *reverse (struct node *head, int k)
+{ 
+    
+    int c=k;
+    node*curr=head;
+    node*next=NULL;
+    node*prev=NULL;
+    while(c-- && curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    if(head!=NULL)
+    head->next=reverse(next,k);
+    return prev;
+}
