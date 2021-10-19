e3d872023d2baebad2af57efcc984677bdb9e405
Kritika Jain <72225751+Kritika6100@users.noreply.github.com>
2021-10-19 14:52:10 +0530

    Update Reverseanarrayingroupsofgivensize.java

--- Arrays/Kadane's algorithm/Reverseanarrayingroupsofgivensize.java
+++ Arrays/Kadane's algorithm/Reverseanarrayingroupsofgivensize.java
@@ -1,3 +1,28 @@
+Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+
+	
+	 
+Input: 
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+k = 3 
+Output: 
+[3, 2, 1, 6, 5, 4, 9, 8, 7]
+
+Input: 
+arr = [1, 2, 3, 4, 5, 6, 7, 8] 
+k = 5 
+Output: 
+[5, 4, 3, 2, 1, 8, 7, 6]
+
+Input: 
+arr = [1, 2, 3, 4, 5, 6] 
+k = 1 
+Output: 
+[1, 2, 3, 4, 5, 6] 
+
+
+
+
 // Java program to reverse every sub-array formed by
 // consecutive k elements
 class solution {
