acf84328c251124f1872cf3e90c8ec7e0d133bc0
Nayan Vishwakarma <52904138+nayan1xyz@users.noreply.github.com>
2021-10-24 15:07:31 +0530

    Added Problem Statement
    I have added Problem Statement For the question Container With Most Water.

--- Arrays/Two Pointer Algorithm/Container-With-Most-Water.cpp
+++ Arrays/Two Pointer Algorithm/Container-With-Most-Water.cpp
@@ -1,11 +1,32 @@
 // C++ code for Max
 // Container-with-most-water
+
+// Problem Statement
+
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+// n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). 
+// Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+
+// Input: height = [1,1]
+// Output: 1
+
+
+
 // Complexity Analysis: 
 
 // Time Complexity: O(n). 
 // As only one traversal of the array is required, so time complexity is O(n).
 // Space Complexity: O(1). 
 // No extra space is required, so space complexity is constant.
+
+
+
 
 #include<iostream>
 using namespace std;
