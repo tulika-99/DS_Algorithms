d23dea4aa846f723b1e9fd558e94348940ec67f5
Adrika Gupta <adrikagupta22@gmail.com>
2021-10-31 18:21:41 +0530

    detect euler circuit in graph: cpp solution and problem statement added

--- Graphs/Euler Circuit/euler_circuit.cpp
+++ Graphs/Euler Circuit/euler_circuit.cpp
@@ -1,3 +1,32 @@
+/*
+Problem statement
+Eulerian Path is a path in graph that visits every edge exactly once. Eulerian Circuit is an Eulerian Path which starts and ends on the same vertex. The task is to find that there exists the Euler Path or circuit or none in given undirected graph with V vertices and adjacency list adj.
+
+Example 1:
+
+Input: 
+
+Output: 2
+Explanation: The graph contains Eulerian circuit
+Example 2:
+
+Input: 
+
+Output: 1
+Explanation: The graph contains Eulerian path.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isEulerCircuilt() which takes number of vertices in the graph denoting as V and adjacency list of graph denoting as adj and returns 1 if graph contains Eulerian Path, 2 if graph contains Eulerian Circuit 0 otherwise.
+ 
+
+Expected Time Complexity: O(V+E) where E is the number of edges in graph.
+Expected Space Complexity: O(V)
+ 
+
+Constraints:
+1 ≤ V, E ≤ 104
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -59,6 +88,3 @@ int main(){
 		cout << ans <<"\n";	}
 	return 0;
 }  // } Driver Code Ends
-/*
-task is to complete the function isEulerCircuilt() which takes number of vertices in the graph denoting as V and adjacency list of graph denoting as adj and returns 1 if graph contains Eulerian Path, 2 if graph contains Eulerian Circuit 0 otherwise.
-*/
