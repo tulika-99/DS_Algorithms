9a5dab7cf88d18690c174f12617d9a1a3345e7fb
Ivan Bibik <ivan.bibik@bimeister.com>
2022-10-10 20:25:31 +0300

    gcd c# implementation

---
+++ Number Theory/GCD/gcd-csharp.cs
@@ -0,0 +1,33 @@
+﻿namespace Gcd
+{
+    internal class Program
+    {
+        static int Gcd(int first, int second)
+        {
+            while (first != second)
+            {
+                if (first > second)
+                {
+                    first = first - second;
+                }
+                else
+                {
+                    second = second - first;
+                }
+            }
+
+            return second;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter first integer number:");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer number:");
+            var second = Convert.ToInt32(Console.ReadLine());
+
+            var gcd = Gcd(first, second);
+            Console.WriteLine($"GCD: {gcd}");
+        }
+    }
+}
